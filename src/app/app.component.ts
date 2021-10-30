b4bf332041d5a2d276d84b2b5175c1fcddcf3349
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-10-30 15:56:21 -0500

    http exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-angular-http-request-exercises';
+  myMovies = [];
+
+
+  constructor(private http: HttpClient){
+
+  }
+
+  onSubmit(form){
+    console.log(form)
+    this.sendData(form.value)
+    };
+
+  sendData(createdMovie: {title: string; genre: string }){
+    this.http.post('https://movies-project-8b820-default-rtdb.firebaseio.com/movies.json',
+    createdMovie
+    ).subscribe(responseData => {
+      console.log(responseData)
+    });
+  }
+  onRecieveMovies(){
+    this.http.get('https://movies-project-8b820-default-rtdb.firebaseio.com/movies.json')
+    .pipe(map(responseData => {
+      const myMovies = [];
+      for(const elem in responseData){
+        myMovies.push(responseData[elem]);
+      }
+      return myMovies;
+    })).subscribe(stuff => {
+      this.myMovies = stuff
+      ;
+    })
+  }
+
 }
+
